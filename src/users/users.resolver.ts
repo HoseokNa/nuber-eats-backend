bc643d2c2c5a866b2b7bf6cc25aa7b4cede5bd42
Hoseok Na <hsna7024@gmail.com>
2021-03-09 23:06:25 +0900

    Create Account Mutation

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,8 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dtos/create-account.dto';
 import { User } from './entities/user.entity';
 import { UserService } from './users.service';
 
@@ -9,5 +13,25 @@ export class UserResolver {
   @Query((returns) => Boolean)
   hi() {
     return true;
+  }
+
+  @Mutation((returns) => CreateAccountOutput)
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    try {
+      const { ok, error } = await this.usersService.createAccount(
+        createAccountInput,
+      );
+      return {
+        ok,
+        error,
+      };
+    } catch (error) {
+      return {
+        error,
+        ok: false,
+      };
+    }
   }
 }
